11c6f0f3963c435fbbc0e72e1e7fa6474be1414b
sansaha <sansaha@SANSAHAWIN7C.ad.indiadev.lexmark.com>
2016-04-13 16:45:20 +0530

    updated sorting for promotions

--- apollo-api/src/main/java/com/lexmark/apollo/api/service/queries/PromotionQuery.java
+++ apollo-api/src/main/java/com/lexmark/apollo/api/service/queries/PromotionQuery.java
@@ -3,6 +3,6 @@ package com.lexmark.apollo.api.service.queries;
 public class PromotionQuery {
     
     public static final String SELECT_PROMOTIONS_BETWEEN_DATES_QUERY = "select * from lexmark_prod.promotions where startdate between :startDate and :endDate union "
-            + "select * from lexmark_prod.promotions where startdate < :startDate and enddate is null or enddate between :startDate and :endDate order by startdate desc";
+            + "select * from lexmark_prod.promotions where startdate < :startDate and enddate is null or enddate between :startDate and :endDate";
 
 }
